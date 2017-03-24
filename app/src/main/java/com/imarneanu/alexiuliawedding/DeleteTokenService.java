140844c54d98b9958a3c23e58caef391bf3e3f6d
imarneanu <iulia.marneanu@p3-group.com>
2017-03-24 14:02:16 +0200

    Initial commit. Using FCM

---
+++ app/src/main/java/com/imarneanu/alexiuliawedding/DeleteTokenService.java
@@ -0,0 +1,46 @@
+package com.imarneanu.alexiuliawedding;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by iulia on 24/03/2017.
+ */
+
+public class DeleteTokenService extends IntentService {
+    public static final String TAG = DeleteTokenService.class.getSimpleName();
+
+    public DeleteTokenService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            // Check for current token
+//            String originalToken = getTokenFromPrefs();
+//            Log.d(TAG, "Token before deletion: " + originalToken);
+
+            // Resets Instance ID and revokes all tokens.
+            FirebaseInstanceId.getInstance().deleteInstanceId();
+
+            // Clear current saved token
+//            saveTokenToPrefs("");
+
+            // Check for success of empty token
+//            String tokenCheck = getTokenFromPrefs();
+//            Log.d(TAG, "Token deleted. Proof: " + tokenCheck);
+
+            // Now manually call onTokenRefresh()
+            Log.d(TAG, "Getting new token");
+            FirebaseInstanceId.getInstance().getToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/imarneanu/alexiuliawedding/MainActivity.java
@@ -0,0 +1,16 @@
+package com.imarneanu.alexiuliawedding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Intent deleteTokenService = new Intent(this, DeleteTokenService.class);
+//        startService(deleteTokenService);
+    }
+}
